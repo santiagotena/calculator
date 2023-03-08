d58932237cc7d4b3a97b419d6ad6789ec8c528d8
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-08 09:25:23 +0100

    Add operation funcs and display

---
+++ src/script.ts
@@ -0,0 +1,48 @@
+const memory: {
+	currentSelection: string,
+	lastSelection: string,
+	lastResult: number
+} = {
+	currentSelection: "",
+    lastSelection: "",
+    lastResult: 0,
+};
+
+function add(a: number, b: number): number {
+	return (a + b);
+}
+
+function substract(a: number, b:number): number {
+	return (a - b);
+}
+
+function multiply(a: number, b: number): number {
+	return (a * b);
+}
+
+function divide(a: number, b: number): number {
+	return (a / b);
+ }
+
+ function operate(a: number, b: number, operation: string): number {
+	if (operation == "add")
+		return (add(a, b));
+	else if (operation == "substract")
+		return (substract(a, b));
+	else if (operation == "multiply")
+		return (multiply(a, b));
+	else if (operation == "divide")
+		return (divide(a, b));
+ }
+
+ function displayChoice(key: string): void {
+	let display = document.querySelector('.display');
+	display.textContent = key;
+ }
+
+ let selections = document.querySelectorAll('.key');
+ selections.forEach((selection): void => {
+	selection.addEventListener('click', (): void => {
+		displayChoice(selection.getAttribute('data-key'));
+	})
+ })
