5e5cfaa41c6ade6a31a0d0ecdb6c42257c0e9fb3
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-11 09:42:11 +0100

    Complete processAns

--- js/utils.js
+++ js/utils.js
@@ -6,12 +6,31 @@ let resetMemory = memoryImport.resetMemory;
 import * as displayImport from './display.js';
 let displayScreen = displayImport.displayScreen;
 let resetDisplay = displayImport.resetDisplay;
+import * as processSelectionImport from './process-selection.js';
+let isLastResult = processSelectionImport.isLastResult;
+let isFirstNumber = processSelectionImport.isFirstNumber;
+let isOperator = processSelectionImport.isOperator;
+let isSecondNumber = processSelectionImport.isSecondNumber;
 function clear() {
     resetMemory();
     resetDisplay();
 }
 function processAns() {
-    if (memory.firstNumber == undefined) {
-        // displayScreen(memory.displayString);
+    if (!isLastResult())
+        memory.lastResult = 0;
+    if (!isFirstNumber()) {
+        memory.firstNumber = memory.lastResult;
+        memory.displayString = "Ans";
+        displayScreen(memory.displayString);
+    }
+    else if (isFirstNumber() && isOperator() && !isSecondNumber()) {
+        memory.secondNumber = memory.lastResult;
+        memory.displayString = memory.displayString.concat(" Ans");
+        displayScreen(memory.displayString);
+    }
+    else if (isLastResult() && !isOperator() && !isSecondNumber()) {
+        memory.firstNumber = memory.lastResult;
+        memory.displayString = "Ans";
+        displayScreen(memory.displayString);
     }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -7,10 +7,14 @@ import * as memoryImport from './memory.js';
 let memory = memoryImport.memory;
 let oldMemory = memoryImport.memory;
 let resetMemory = memoryImport.resetMemory;
-
 import * as displayImport from './display.js';
 let displayScreen = displayImport.displayScreen;
 let resetDisplay = displayImport.resetDisplay;
+import * as processSelectionImport from './process-selection.js';
+let isLastResult = processSelectionImport.isLastResult;
+let isFirstNumber = processSelectionImport.isFirstNumber;
+let isOperator = processSelectionImport.isOperator;
+let isSecondNumber = processSelectionImport.isSecondNumber;
 
 function clear(): void {
 	resetMemory();
@@ -18,7 +22,22 @@ function clear(): void {
 }
 
 function processAns(): void {
-	if (memory.firstNumber == undefined) {
-		// displayScreen(memory.displayString);
+	if (!isLastResult())
+		memory.lastResult = 0;
+	if (!isFirstNumber()) {
+		memory.firstNumber = memory.lastResult;
+		memory.displayString = "Ans";
+		displayScreen(memory.displayString);
+		
+	} 
+	else if (isFirstNumber() && isOperator() && !isSecondNumber()) {
+		memory.secondNumber = memory.lastResult;
+		memory.displayString = memory.displayString.concat(" Ans");
+		displayScreen(memory.displayString);
+	}
+	else if (isLastResult() && !isOperator() && !isSecondNumber()) {
+		memory.firstNumber = memory.lastResult;
+		memory.displayString = "Ans";
+		displayScreen(memory.displayString);
 	}
 }
