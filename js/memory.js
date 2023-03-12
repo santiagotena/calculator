c20a86c6bd7bf8b2d6a46b0c3e8adf416349a574
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-13 00:44:15 +0100

    Replace Interface with a class

--- js/memory.js
+++ js/memory.js
@@ -1,39 +1,35 @@
 export { memory, memoryHistory, resetMemory, copyToHistory, copyFromHistory };
-const memory = {
-    firstNumber: undefined,
-    operator: undefined,
-    secondNumber: undefined,
-    lastResult: undefined,
-    isNegative: false,
-    isDecimal: false,
-    decimalSpaces: 0,
-    usedAns: false,
-    usedEqual: false,
-    displayString: "",
-    memorySteps: 1,
-    reachedStart: false,
-};
-const backupMemory = {
-    firstNumber: undefined,
-    operator: undefined,
-    secondNumber: undefined,
-    lastResult: undefined,
-    isNegative: false,
-    isDecimal: false,
-    decimalSpaces: 0,
-    usedAns: false,
-    usedEqual: false,
-    displayString: "",
-    memorySteps: 1,
-    reachedStart: false,
-};
-let memoryHistory = [memory];
+class Memory {
+    constructor(firstNumber = undefined, operator = undefined, secondNumber = undefined, lastResult = undefined, isNegative = false, isDecimal = false, decimalSpaces = 0, usedAns = false, usedEqual = false, displayString = "", memorySteps = 1, reachedStart = false) {
+        this.firstNumber = firstNumber;
+        this.operator = operator;
+        this.secondNumber = secondNumber;
+        this.lastResult = lastResult;
+        this.isNegative = isNegative;
+        this.isDecimal = isDecimal;
+        this.decimalSpaces = decimalSpaces;
+        this.usedAns = usedAns;
+        this.usedEqual = usedEqual;
+        this.displayString = displayString;
+        this.memorySteps = memorySteps;
+        this.reachedStart = reachedStart;
+    }
+}
+function makeMemoryHistory(n) {
+    let memoryHistory = new Array(n);
+    for (let i = 0; i < n; ++i) {
+        memoryHistory[i] = new Memory();
+    }
+    return (memoryHistory);
+}
+const memory = new Memory();
+const backupMemory = new Memory();
+const memoryHistory = makeMemoryHistory(100);
 function resetMemory() {
     for (let key in memory)
         memory[key] = backupMemory[key];
 }
 function copyToHistory() {
-    memoryHistory.push(memory);
     for (let key in memory)
         memoryHistory[memory.memorySteps - 1][key] = memory[key];
 }
--- src/memory.ts
+++ src/memory.ts
@@ -6,52 +6,60 @@ export {
 		copyFromHistory
 	};
 
-interface MemoryInterface {
-    firstNumber: number,
-	operator: string,
-	secondNumber: number,
-	lastResult: number,
-	isNegative: boolean,
-	isDecimal: boolean,
-	decimalSpaces: number,
-	usedAns: boolean,
-	usedEqual: boolean,
-	displayString: string,
-	memorySteps: number,
-	reachedStart: boolean
+class Memory {
+	firstNumber: number;
+	operator: string;
+	secondNumber: number;
+	lastResult: number;
+	isNegative: boolean;
+	isDecimal: boolean;
+	decimalSpaces: number;
+	usedAns: boolean;
+	usedEqual: boolean;
+	displayString: string;
+	memorySteps: number;
+	reachedStart: boolean;
+
+	constructor(
+		firstNumber = undefined, 
+		operator = undefined,
+		secondNumber = undefined,
+		lastResult = undefined,
+		isNegative = false,
+		isDecimal = false,
+		decimalSpaces = 0,
+		usedAns = false,
+		usedEqual = false,
+		displayString = "",
+		memorySteps = 1,
+		reachedStart = false
+	) {
+		this.firstNumber = firstNumber;
+		this.operator = operator;
+		this.secondNumber = secondNumber;
+		this.lastResult = lastResult;
+		this.isNegative = isNegative;
+		this.isDecimal = isDecimal;
+		this.decimalSpaces = decimalSpaces;
+		this.usedAns = usedAns;
+		this.usedEqual = usedEqual;
+		this.displayString = displayString;
+		this.memorySteps = memorySteps;
+		this.reachedStart = reachedStart;
+	}
+}
+	
+function makeMemoryHistory(n: number) {
+	let memoryHistory = new Array(n)
+	for (let i = 0; i < n; ++i) {
+		memoryHistory[i] = new Memory()
+	}
+	return (memoryHistory);
 }
 
-const memory: MemoryInterface = {
-    firstNumber: undefined,
-	operator: undefined,
-	secondNumber: undefined,
-    lastResult: undefined,
-	isNegative: false,
-	isDecimal: false,
-	decimalSpaces: 0,
-	usedAns: false,
-	usedEqual: false,
-	displayString: "",
-	memorySteps: 1,
-	reachedStart: false,
-}
-
-const backupMemory: MemoryInterface = {
-    firstNumber: undefined,
-	operator: undefined,
-	secondNumber: undefined,
-    lastResult: undefined,
-	isNegative: false,
-	isDecimal: false,
-	decimalSpaces: 0,
-	usedAns: false,
-	usedEqual: false,
-	displayString: "",
-	memorySteps: 1,
-	reachedStart: false,
-}
-
-let memoryHistory = [memory];
+const memory = new Memory();
+const backupMemory = new Memory();
+const memoryHistory = makeMemoryHistory(100);
 
 function resetMemory(): void {
 	for (let key in memory)
@@ -59,7 +67,6 @@ function resetMemory(): void {
 }
 
 function copyToHistory(): void {
-	memoryHistory.push(memory);
 	for (let key in memory)
 		memoryHistory[memory.memorySteps - 1][key]= memory[key];
 }
