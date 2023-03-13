758b51daf1fc7c43f50294246525a8453d4c506b
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-13 13:38:55 +0100

    Refactor operator.ts

--- src/process-selection/operator.ts
+++ src/process-selection/operator.ts
@@ -2,68 +2,87 @@ import { isFirstNumber, isOperator, isSecondNumber } from "./booleans.js";
 import { memory } from "../memory.js";
 import { displayScreen } from "../display.js";
 
+function addOperatorToZero(selectionType: string): void {
+	if (selectionType == "+") {
+		memory.firstNumber = 0;
+		memory.displayString = "0 + ";
+	}
+	if (selectionType == "*") {
+		memory.firstNumber = 0;
+		memory.displayString = "0 x ";
+	}
+	if (selectionType == "/") {
+		memory.firstNumber = 0;
+		memory.displayString = "0 ÷ ";
+	}
+	memory.operator = selectionType;
+	displayScreen(memory.displayString);
+}
+
+function makeFirstNumberNegative(): void {
+	memory.isNegative = true;
+	memory.displayString = "-";
+	displayScreen(memory.displayString);
+}
+
+function placeOperator(selectionType: string): void {
+	if (selectionType == "-")
+		memory.displayString = memory.displayString.concat(" - ");
+	if (selectionType == "+")
+		memory.displayString = memory.displayString.concat(" + ");
+	if (selectionType == "*")
+		memory.displayString = memory.displayString.concat(" x ");
+	if (selectionType == "/")
+		memory.displayString = memory.displayString.concat(" ÷ ");
+	memory.operator = selectionType;
+	memory.isDecimal = false;
+	memory.decimalSpaces = 0;
+	displayScreen(memory.displayString);
+}
+
+function replaceOperator(selectionType: string): void {
+	memory.displayString = memory.displayString.slice(0, -2);
+	if (selectionType == "-")
+		memory.displayString = memory.displayString.concat("- ");
+	if (selectionType == "+")
+		memory.displayString = memory.displayString.concat("+ ");
+	if (selectionType == "*")
+		memory.displayString = memory.displayString.concat("x ");
+	if (selectionType == "/")
+		memory.displayString = memory.displayString.concat("÷ ");
+	memory.operator = selectionType;
+	displayScreen(memory.displayString);
+}
+
+function makeSecondNumberNegative(): void {
+	memory.isNegative = true;
+	memory.displayString = memory.displayString.concat("-");
+	displayScreen(memory.displayString);
+}
+
 function processOperator(selectionType: string): number {
 	if (!isFirstNumber() || (memory.firstNumber == 0 && isOperator())) {
 		if (selectionType == "-") {
-			memory.isNegative = true;
-			memory.displayString = "-";
-			displayScreen(memory.displayString);
+			makeFirstNumberNegative();
 			return (0);
 		}
-		if (memory.isNegative == false)
-		{
-			if (selectionType == "+") {
-				memory.firstNumber = 0;
-				memory.displayString = "0 +";
-			}
-			if (selectionType == "*") {
-				memory.firstNumber = 0;
-				memory.displayString = "0 x";
-			}
-			if (selectionType == "/") {
-				memory.firstNumber = 0;
-				memory.displayString = "0 ÷";
-			}
+		if (!memory.isNegative) {
+			addOperatorToZero(selectionType);
+			return (0);
 		}
-		memory.operator = selectionType;
-		displayScreen(memory.displayString);
+	} else if (!isOperator()) {
+		placeOperator(selectionType);
 		return (0);
 	} else if (isOperator() && !isSecondNumber()) {
 		if (selectionType == "-") {
 			if (memory.isNegative)
 				return (1);
-			memory.isNegative = true;
-			memory.displayString = memory.displayString.concat("-");
-			displayScreen(memory.displayString);
+			makeSecondNumberNegative();
 			return (0);
+		} else {
+			replaceOperator(selectionType);
+			return (1);
 		}
-	} else if (!isOperator()) {
-		if (selectionType == "-")
-			memory.displayString = memory.displayString.concat(" - ");
-		if (selectionType == "+")
-			memory.displayString = memory.displayString.concat(" + ");
-		if (selectionType == "*")
-			memory.displayString = memory.displayString.concat(" x ");
-		if (selectionType == "/")
-			memory.displayString = memory.displayString.concat(" ÷ ");
-		memory.operator = selectionType;
-		memory.isDecimal = false;
-		memory.decimalSpaces = 0;
-		displayScreen(memory.displayString);
-		return (0);
-	} else if (!isSecondNumber()) {
-		memory.displayString = memory.displayString.slice(0, -1);
-		if (selectionType == "-")
-			memory.displayString = memory.displayString.concat("-");
-		if (selectionType == "+")
-			memory.displayString = memory.displayString.concat("+");
-		if (selectionType == "*")
-			memory.displayString = memory.displayString.concat("x");
-		if (selectionType == "/")
-			memory.displayString = memory.displayString.concat("÷");
-		memory.operator = selectionType;
-		displayScreen(memory.displayString);
-		return (1);
 	}
 }
 
