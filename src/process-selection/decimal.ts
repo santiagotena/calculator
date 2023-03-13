0e70affdab2a61a65ca3a8897ae130ce253364bf
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-13 14:07:34 +0100

    Refactor decimal.ts

--- src/process-selection/decimal.ts
+++ src/process-selection/decimal.ts
@@ -2,35 +2,42 @@ import { isFirstNumber, isOperator, isSecondNumber } from "./booleans.js";
 import { memory } from "../memory.js";
 import { displayScreen } from "../display.js";
 
+function addDecimalToFirstNumber(selectionNumber: number): void {
+	if (memory.firstNumber < 0)
+		memory.firstNumber = memory.firstNumber - selectionNumber / 10 ** memory.decimalSpaces;
+	else	
+		memory.firstNumber = memory.firstNumber + selectionNumber / 10 ** memory.decimalSpaces;
+	memory.firstNumber = Math.round((memory.firstNumber + Number.EPSILON) * 100) / 100;
+	if (memory.isNegative) {
+		memory.firstNumber = memory.firstNumber * -1;
+		if (memory.firstNumber != 0)
+			memory.isNegative = false;
+	}
+	memory.displayString = memory.displayString.concat(selectionNumber.toString());
+}
+
+function addDecimalToSecondNumber(selectionNumber: number): void {
+	if (memory.secondNumber < 0)
+		memory.secondNumber = memory.secondNumber - selectionNumber / 10 ** memory.decimalSpaces;
+	else	
+		memory.secondNumber = memory.secondNumber + selectionNumber / 10 ** memory.decimalSpaces;
+	memory.secondNumber = Math.round((memory.secondNumber + Number.EPSILON) * 100) / 100;
+	if (memory.isNegative) {
+		memory.secondNumber = memory.secondNumber * -1;
+		if (memory.secondNumber != 0)
+			memory.isNegative = false;
+	}
+	memory.displayString = memory.displayString.concat(selectionNumber.toString());
+}
+
 function processDecimal(selectionKey: string): number {
 	let selectionNumber: number;
 
 	selectionNumber = +selectionKey;
-	if (isFirstNumber() && !isOperator() && !isSecondNumber()) {
-		if (memory.firstNumber < 0)
-			memory.firstNumber = memory.firstNumber - selectionNumber / 10**memory.decimalSpaces;
-		else	
-			memory.firstNumber = memory.firstNumber + selectionNumber / 10**memory.decimalSpaces;
-		memory.firstNumber = Math.round((memory.firstNumber + Number.EPSILON) * 100) / 100;
-		if (memory.isNegative) {
-			memory.firstNumber = memory.firstNumber * -1;
-			if (memory.firstNumber != 0)
-				memory.isNegative = false;
-		}
-		memory.displayString = memory.displayString.concat(selectionNumber.toString());
-	} else if (isFirstNumber() && isOperator() && isSecondNumber()) {
-		if (memory.secondNumber < 0)
-			memory.secondNumber = memory.secondNumber - selectionNumber / 10**memory.decimalSpaces;
-		else	
-			memory.secondNumber = memory.secondNumber + selectionNumber / 10**memory.decimalSpaces;
-		memory.secondNumber = Math.round((memory.secondNumber + Number.EPSILON) * 100) / 100;
-		if (memory.isNegative) {
-			memory.secondNumber = memory.secondNumber * -1;
-			if (memory.secondNumber != 0)
-				memory.isNegative = false;
-		}
-		memory.displayString = memory.displayString.concat(selectionNumber.toString());
-	}
+	if (isFirstNumber() && !isOperator() && !isSecondNumber())
+		addDecimalToFirstNumber(selectionNumber);
+	else if (isFirstNumber() && isOperator() && isSecondNumber())
+		addDecimalToSecondNumber(selectionNumber);
 	memory.decimalSpaces++;
 	displayScreen(memory.displayString);
 	return (0);
@@ -39,15 +46,17 @@ function processDecimal(selectionKey: string): number {
 function addDot(): number {
 	if (memory.isDecimal)
 		return (1);
-	if (!isOperator() && !isSecondNumber() && (memory.usedAns || memory.usedEqual || memory.reachedStart)) {
+	if (
+		!isOperator() && 
+		!isSecondNumber() && 
+		(memory.usedAns || memory.usedEqual || memory.reachedStart)
+		) {
 		memory.displayString = "";
 		memory.firstNumber = 0;
 		memory.displayString = memory.displayString.concat("0.");
 	} else if (!isFirstNumber()) {
 		memory.firstNumber = 0;
 		memory.displayString = memory.displayString.concat("0.");
-	} else if (!isOperator() && !isSecondNumber()) {
-		memory.displayString = memory.displayString.concat(".");
 	} else if (isOperator() && !isSecondNumber()) {
 		memory.secondNumber = 0;
 		memory.displayString = memory.displayString.concat("0.");
